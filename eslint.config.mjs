424f372f216e8e27d2cf61e263fb75bec9752b5e
Achira <achira00decisioner@gmail.com>
2026-02-19 02:07:01 +0700

    gg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,8 @@ const eslintConfig = [
     rules: {
       "@next/next/no-img-element": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "prefer-const": "warn",
     },
   },
 ];
